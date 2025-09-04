5306cfa035fe3606282b58df690e3c9057047c76
m334701 <m334701@one.merckgroup.com>
2025-09-04 21:29:09 +0530

    temp modification

--- src/main/java/com/tinysteps/serviceregistry/ServiceRegistryApplication.java
+++ src/main/java/com/tinysteps/serviceregistry/ServiceRegistryApplication.java
@@ -1,4 +1,4 @@
-package com.tintsteps.serviceregistry;
+package com.tinysteps.serviceregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/tintsteps/serviceregistry/ServiceRegistryApplicationTests.java
+++ src/test/java/com/tintsteps/serviceregistry/ServiceRegistryApplicationTests.java
@@ -1,4 +1,4 @@
-package com.tintsteps.serviceregistry;
+package com.tinysteps.serviceregistry;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
